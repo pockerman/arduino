876b8a00279b740852e7fa5939d47cc5a083f8e2
David Hilbert <a.giavaras@gmail.com>
2017-12-30 19:49:40 +0200

    flashing_leds2/flashing_leds2.ino V1

--- flashing_leds2/flashing_leds2.ino
+++ flashing_leds2/flashing_leds2.ino
@@ -1,13 +1,17 @@
 /**
- * This tutorial uses Arduino to flash a given number of LEDs
+ * This tutorial uses Arduino to flash a given number of LEDs. It also
+ * assumes that a button is used that is dealt via an interrupt
  */
 
+volatile int state = LOW;
 
  //array with the LDE pins. We use 3 LEDs
  int led_pins[]={8,10,12};
 
  //indicates some delay
  int time_delay = 75;
+
+ int pause = 500;
 
  
 void setup() {
@@ -17,17 +21,41 @@ void setup() {
     pinMode(led_pins[i],OUTPUT);
   }
 
+  //initialize the interrupt
+  attachInterrupt(0, ledOnOff, RISING);
+
 }
 
 void loop() {
 
   //simply loop over the pins
   //and do some work
-  for(int i=0; i<3; i++){
-    digitalWrite(led_pins[i],HIGH);
-    delay(time_delay);
-    digitalWrite(led_pins[i], LOW);
-    delay(time_delay);
-  }
+ if (state) {
+    for (int i=0; i<3; i++){
+      digitalWrite(led_pins[i], HIGH);
+      delay(time_delay);
+    }
+  
+    delay(pause);
+  
+    for (int i=0; i<3; i++){
+      digitalWrite(led_pins[i], LOW);
+      delay(time_delay);
+    }
 
+    delay(pause);
+  }
+}
+
+void ledOnOff() {
+  static unsigned long lastMillis = 0;
+  unsigned long newMillis = millis();
+
+  //perform Debounce check
+  if (newMillis - lastMillis < 50){
+  }
+  else {
+    state = !state;
+    lastMillis = newMillis;
+  }
 }
