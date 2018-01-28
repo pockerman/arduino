25c6780ebb758d4ac38b167bac61157b32ca358f
David Hilbert <a.giavaras@gmail.com>
2018-01-28 19:09:21 +0200

    Added hc_sr04_new_ping/

---
+++ hc_sr04_new_ping/README.md
@@ -0,0 +1,15 @@
+
+# HC-SR04 Ultrasound Sensor Using NewPing Library
+
+Shows how to use the HC-SR04 ultrasound sensor
+
+## Topics
+
+- HC-SR04 ultrasound sensor 
+
+## References
+
+- **Tutorial:** 
+- **HC-SR04 ultrasound sensor:**  
+
+
---
+++ hc_sr04_new_ping/hc_sr04_new_ping.ino
@@ -0,0 +1,20 @@
+/**
+ * This sketch shows how to use the HC-SR04 sensor
+ * with the NewPing library
+ */
+
+#include "NewPing.h"
+
+const int TRIG_PIN = 10;
+const int ECHO_PIN = 13;
+float duration = 0.0f;
+float distance = 0.0f;
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
