d5faf361e49f2a1852b52e082013108e28de10c9
David Hilbert <a.giavaras@gmail.com>
2018-02-03 18:46:47 +0200

    Added test_isr_2_motors/

---
+++ test_isr_2_motors/README.md
@@ -0,0 +1,18 @@
+
+# Test ISR for two motors
+
+Shows how to use Arduino's ISR to control two motors.
+
+## Topics
+
+- ISR
+- ```TimerOne``` library
+
+
+## References
+
+- **Tutorial:** 
+- **Using Arduino ISR** https://www.allaboutcircuits.com/technical-articles/using-interrupts-on-arduino/
+- ```TimerOne``` library: https://playground.arduino.cc/Code/Timer1
+
+
---
+++ test_isr_2_motors/test_isr_2_motors.ino
@@ -0,0 +1,28 @@
+
+
+/**
+ * Test ISR for two motors. The sketch has been taken from
+ * the following YouTub tutorial:
+ * 
+ * https://www.youtube.com/watch?v=oQQpAACa3ac&t=2s
+ */
+
+#include "TimerOne.h"
+
+const byte MTR1 = 2; //Motor 1 interrupt pin INT 0
+const byte MTR2 = 3; //Motor 2 interrupt pin INT 1
+
+//pulse counters
+unsigned int counter1 = 0;
+unsigned int counter2 = 0;
+ 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
