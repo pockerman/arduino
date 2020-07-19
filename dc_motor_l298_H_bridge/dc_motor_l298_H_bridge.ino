5af3e21afb663084f88a5a2e3e511b0d14ba32dc
pockerman <a.giavaras@gmail.com>
2020-07-19 13:29:29 +0100

    Update dc motor sketch

--- dc_motor_l298_H_bridge/dc_motor_l298_H_bridge.ino
+++ dc_motor_l298_H_bridge/dc_motor_l298_H_bridge.ino
@@ -1,15 +1,46 @@
 
-const int IN1 = 7;
-const int IN2 = 6;
-const int ENA = 11;
+const int IN1 = 8;
+const int IN2 = 7;
 
+// enable pin must be able to use PWM
+const int ENA = 9;
+const int speed = 200;
 
 void setup() {
-  // put your setup code here, to run once:
+
+  // these are comming out of the
+  // UNO board and into the L298N module
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
+
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  Serial.println("Forward...");
+
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, speed);
+
+  //run the test for 2 seconds
+  delay(2000);
+
+  Serial.println("Backward...");
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, speed);
+
+  //run the test for 2 seconds
+  delay(2000);
+
+  // stop the motor
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, LOW);
 
 }
