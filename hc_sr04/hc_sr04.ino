0f15a2becb388b98938d2941683bea07c0bc1685
pockerman <a.giavaras@gmail.com>
2020-07-25 13:07:37 +0100

    Fix documentation strings

--- hc_sr04/hc_sr04.ino
+++ hc_sr04/hc_sr04.ino
@@ -1,7 +1,8 @@
-/**
- * This sketch shows how to use the HC-SR04 sensor
- */
+///
+/// This sketch shows how to use the HC-SR04 sensor
+///
 
+ 
 const int TRIG_PIN = 10;
 const int ECHO_PIN = 13;
 float duration = 0.0f;
@@ -10,7 +11,8 @@ float distance = 0.0f;
 void setup() {
 
   Serial.begin(9600);
-  //need to setip the pins first
+
+
   pinMode(TRIG_PIN,OUTPUT);
   pinMode(ECHO_PIN,INPUT);
 
@@ -18,38 +20,38 @@ void setup() {
 
 void loop() {
 
-  //write a pulse to the HC-SR04 trigger pin
-  //effectively creates a 10 microsecond pulse
+  /// write a pulse to the HC-SR04 trigger pin
+  /// effectively creates a 10 microsecond pulse
   digitalWrite(TRIG_PIN,LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN,HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN,LOW);
 
-  //Measure the response from the HC-SR04 echo pin
-  //measure how long the pulse stays high
+  /// Measure the response from the HC-SR04 echo pin
+  /// measure how long the pulse stays high
   duration = pulseIn(ECHO_PIN,HIGH);
 
-  /*
-   * Determine distance from duration. Use 343 meters 
-   * per second as speed of sound. Duration is divided by two 
-   * as the signal also includes the return trip time
-   */
-   distance = (duration*0.5)*0.0343;
+  /// Determine distance from duration. Use 343 meters 
+  /// per second as speed of sound. Duration is divided by two 
+  /// as the signal also includes the return trip time
+   
+  distance = (duration*0.5)*0.0343;
 
-   //use the serial monitor to print the results
-   Serial.print("Distance is = ");
+  /// use the serial monitor to print the results
+  Serial.print("Distance is = ");
 
-  //the sensor has maximum 400 cm and minimum 2 cm
-  //anything outside this range is invalid
-   if(distance >= 400. || distance <= 2.0){
+  /// the sensor has maximum 400 cm and minimum 2 cm
+  /// anything outside this range is invalid
+  if(distance >= 400. || distance <= 2.0){
 
       Serial.print("Out of range");
-   }
-   else{
+  }
+  else{
+    
     Serial.print(distance);
     Serial.print(" cm");
     
-   }
+  }
 
 }
